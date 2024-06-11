2ec3f1971cdb1c8776973d130599aef87d145f4c
YingxinChen <157030399+CassieYXChen@users.noreply.github.com>
2024-06-12 00:22:51 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const submitBtn = document.querySelector('.submitBtn');
+submitBtn.addEventListener('click', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Get user input
+  const charInput = document.querySelector('.char-input');
+  const userInput = charInput.value;
+
+  // Display result
+  const outputContainer = document.querySelector('.output-container');
+  const outputImg = document.querySelector('#output-img');
+  outputImg.setAttribute('src', 'your_output_image_url_here');
+
+  const stampImg = document.querySelector('#stamp');
+  stampImg.setAttribute('src', './front-end/wanxizhi_stamp.png');
+}
