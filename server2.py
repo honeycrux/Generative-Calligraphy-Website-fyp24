070b299f3ef78644224e999d23f6718f98c4b7e8
Esther <hwelee0105@gmail.com>
2024-07-02 09:47:18 +0800

    Updated server2.py, script.js, styles.css, index.html to match linux ver [add loader, tidy functions]

--- server2.py
+++ server2.py
@@ -1,12 +1,82 @@
 from flask import Flask, request, send_from_directory, abort, send_file, render_template
 from flask_cors import CORS
+from flask import jsonify
 import os, subprocess
 
 app = Flask(__name__)
 CORS(app)
 
-# Directory where files are stored
-FILES_DIRECTORY = '/research/d2/fyp23/lylee0/Font-diff/'
+FILES_DIRECTORY = '/research/d2/fyp23/lylee0/Font-diff_content/'
+# Directory where result files are stored
+RES_FILES_DIRECTORY = '/research/d2/fyp23/lylee0/Font-diff_content/result_web'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+# Directory where txt files are stored
+SAVE_DIRECTORY = '/research/d2/fyp23/lylee0/Font-diff_content/'
+
+@app.route('/save-file', methods=['GET'])
+def save_file():
+    try:
+        # Get the content and filename from the request
+        content = request.args.get('content')
+        filename = request.args.get('filename')
+        # print("filenames is.....", filename)
+
+        # Check if the content and filename are provided
+        if not content or not filename:
+            abort(400, description="Invalid request parameters")
+
+        # Save the file
+        file_path = os.path.join(SAVE_DIRECTORY, filename)
+        # print("file_path is.....", file_path)
+        with open(file_path, 'w') as file:
+            file.write(content)
+        # print("file saved")
+
+        # Change permissions of the file
+        # os.chmod(file_path, 0o777)
+        # print("file changed permit")
+
+        return 'File saved successfully.', 200 
+    except Exception as e:
+        abort(500, description=str(e))
+
+@app.route('/font2img', methods=['GET'])
+def font2img():
+    try:
+        environment_name = '/research/d2/fyp23/lylee0/miniconda3/envs/down/bin/python3.9' 
+        command = 'python /research/d2/fyp23/lylee0/Font-diff_content/font2img.py'  # Shell command to be executed
+        # command = f'mpiexec -n 1 python sample.py --cfg_path cfg/test_cfg.yaml'
+
+        result = run_command(command)
+        # result = subprocess.check_output(command, shell=True)
+        # result = os.popen(command).read()
+        return f'Command "{command}" executed in Conda environment "{environment_name}"'
+    except Exception as e:
+        return str(e)
+
+@app.route('/generate_result', methods=['GET'])
+def generate_result():
+    try:
+        environment_name = '/research/d2/fyp23/lylee0/miniconda3/envs/down/bin/python3.9'
+        command = f'mpiexec -n 1 python sample.py --cfg_path cfg/test_cfg.yaml'
+        # command = f'mpiexec -n 1 python sample_backup.py --cfg_path cfg/test_cfg.yaml'
+ 
+        result = run_command(command)
+        return f'Command "{command}" executed in Conda environment "{environment_name}"'
+    except Exception as e:
+        return str(e)
+
+def run_command(command):
+        try:
+            result = subprocess.check_output(command, shell=True, text=True)
+            return result
+        except subprocess.CalledProcessError as e:
+            return f'Error: {e.output}'
 
 @app.route('/files/<filename>', methods=['GET'])
 def get_file(filename):
@@ -19,47 +89,50 @@ def get_file(filename):
     except Exception as e:
         abort(500, description=str(e))
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
-
-# Directory where files are stored
-SAVE_DIRECTORY = '/research/d2/fyp23/lylee0/Font-diff_content/'
-
-@app.route('/save-file', methods=['GET'])
-def save_file():
+@app.route('/return_paths', methods=['GET'])
+def return_result_paths():
+    return_paths = []
+    return_file_names = []
     try:
-        # Get the content and filename from the request
-        content = request.args.get('content')
-        filename = request.args.get('filename')
+        # Check if the directory exists
+        if not os.path.exists(RES_FILES_DIRECTORY):
+            abort(404, description="directory not found")
 
-        # Check if the content and filename are provided
-        if not content or not filename:
-            abort(400, description="Invalid request parameters")
-
-        # Save the file
-        file_path = os.path.join(SAVE_DIRECTORY, filename)
-        with open(file_path, 'w') as file:
-            file.write(content)
-
-        # Change permissions of the file
-        os.chmod(file_path, 0o777)
-
-        return 'File saved successfully.', 200
+        for file in os.listdir(RES_FILES_DIRECTORY):
+            # Check if the file exists
+            if not os.path.isfile(os.path.join(RES_FILES_DIRECTORY, file)):
+                abort(404, description="Result file not found")
+            return_paths.append(os.path.join(RES_FILES_DIRECTORY, file))
+            return_file_names.append(file)
+        
+        return send_from_directory(return_file_names[0])
+        # Send the file paths back to the client
+        # return jsonify(return_paths)
     except Exception as e:
         abort(500, description=str(e))
 
+@app.route('/image/<path:filename>')
+def serve_image(filename):
+    directory = RES_FILES_DIRECTORY  
+    return send_from_directory(directory, filename)
 
-@app.route('/execute_command', methods=['GET'])
-def execute_command():
-    try:
-        command = 'hostname -I'  # Shell command to be executed
-        result = os.popen(command).read()
-        return result
-    except Exception as e:
-        return str(e)
+# Endpoint to fetch the image name or other data
+@app.route('/get_image')
+def get_image():
+    image_names = []
 
+    # Check if the directory exists
+    if not os.path.exists(RES_FILES_DIRECTORY):
+        abort(404, description="directory not found")
+
+    for file in os.listdir(RES_FILES_DIRECTORY):
+        # Check if the img exists
+        if not os.path.isfile(os.path.join(RES_FILES_DIRECTORY, file)):
+            abort(404, description="Result file not found")
+        image_names.append(file)
+
+    return jsonify({'image_name': image_names[0]})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000, debug=True)
+
--- static/script.js
+++ static/script.js
@@ -1,8 +1,12 @@
 const submitBtn = document.querySelector('.submitBtn');
 submitBtn.addEventListener('click', handleSubmit);
 
-function handleSubmit(event) {
+const LOADER_PROCESS_STATUS = "Loading";
+
+async function handleSubmit(event) {
   event.preventDefault();
+
+  update_loader("Loading");
 
   // Get user input
   const charInput = document.querySelector('.char-input');
@@ -14,106 +18,223 @@ function handleSubmit(event) {
   const charContent = charValue;
   const strokeContent = charValue + " " + strokeValue;
 
-  // Save char-input.txt
-  saveFile(charContent, '/research/d2/fyp23/lylee0/Font-diff_content/char-input.txt');
+  console.log("【 handleSubmit() 】Obtained user input and stroke input")
 
-  // Save stroke-input.txt
-  saveFile(strokeContent, '/research/d2/fyp23/lylee0/Font-diff_content/stroke-input.txt');
 
+  try {
+    update_loader("Processing inputs");
+
+    // Save char-input.txt
+    await saveFile(charContent, '/research/d2/fyp23/lylee0/Font-diff_content/char-input.txt');
+
+    console.log("【 handleSubmit() 】Saved user input to /research/d2/fyp23/lylee0/Font-diff_content/char-input.txt")
+
+    // Save stroke-input.txt
+    await saveFile(strokeContent, '/research/d2/fyp23/lylee0/Font-diff_content/stroke-input.txt');
+
+    console.log("【 handleSubmit() 】Saved stroke input to /research/d2/fyp23/lylee0/Font-diff_content/stroke-input.txt")
+
+    update_loader("Preparing for generation");
+
+    // Transform txt to img after both inputs are saved
+    await font2img() 
+
+    console.log("【 handleSubmit() 】Transformed txt to png and saved to /research/d2/fyp23/lylee0/content_folder/")
+
+    update_loader("Generating");
+
+    // Generate result img 
+    await generate_result()
+
+    console.log("【 handleSubmit() 】result generated and saved to /research/d2/fyp23/lylee0/result_web/")
+
+    display_result()
+
+    console.log("【 handleSubmit() 】result displayed; process complete")
+
+  } catch (error) {
+    console.error('Error processing files:', error);
+  }
+  
   // os.system('chmod -R 777 /research/d2/fyp23/lylee0/Font-diff_content/');
-
+  
   // Display result (to be updated)
-  const outputContainer = document.querySelector('.output-container');
-  const outputImg = document.querySelector('#output-img');
-  outputImg.setAttribute('src', ' ');
+  // const outputContainer = document.querySelector('.output-container');
+  // const outputImg = document.querySelector('#output-img');
+  // outputImg.setAttribute('src', ' ');
 
 }
 
 
 // Can save directly on browser
-function saveFileLocally(content, fileName) {
-  const element = document.createElement('a');
-  const file = new Blob([content], { type: 'text/plain' });
-  element.href = URL.createObjectURL(file);
-  element.download = fileName;
-  element.click();
-}
+// function saveFileLocally(content, fileName) {
+//   const element = document.createElement('a');
+//   const file = new Blob([content], { type: 'text/plain' });
+//   element.href = URL.createObjectURL(file);
+//   element.download = fileName;
+//   element.click();
+// }
 
 
 // Use POST method but can only test through postman
-function saveFileUsingPOST(content, filename) {
-const formData = new FormData();
-const blob = new Blob([content], { type: 'text/plain' });
-formData.append('file', blob, filename);
+// function saveFileUsingPOST(content, filename) {
+// const formData = new FormData();
+// const blob = new Blob([content], { type: 'text/plain' });
+// formData.append('file', blob, filename);
 
-fetch('http://137.189.88.154:9000/save-file', {
-method: 'POST',
-body: formData
-})
-.then(function(response) {
-if (response.ok) {
-console.log('File saved successfully.');
-// Perform any additional actions or show success message
-} else {
-console.error('Failed to save file.');
-// Handle the error
-}
-})
-.catch(function(error) {
-console.error('An error occurred:', error);
-// Handle the error
-});
-}
+// fetch('http://137.189.88.153:9000/save-file', {
+// method: 'POST',
+// body: formData
+// })
+// .then(function(response) {
+// if (response.ok) {
+// console.log('File saved successfully.');
+// // Perform any additional actions or show success message
+// } else {
+// console.error('Failed to save file.');
+// // Handle the error
+// }
+// })
+// .catch(function(error) {
+// console.error('An error occurred:', error);
+// // Handle the error
+// });
+// }
 
 
 // Use GET method instead of POST to avoid body missing error
 function saveFile(content, filename) {
   const encodedContent = encodeURIComponent(content);
   const encodedFilename = encodeURIComponent(filename);
-  const url = `http://137.189.88.153:9000/save-file?content=${encodedContent}&filename=${encodedFilename}`;
+  const front_url = construct_url();
+  const req_url = `save-file?content=${encodedContent}&filename=${encodedFilename}`;
+  const url = front_url.concat(req_url)
+  // const url = `http://137.189.88.56:9000/save-file?content=${encodedContent}&filename=${encodedFilename}`;
+  // console.log("obtained url: ", url)
 
-  fetch(url)
+
+  return new Promise((resolve, reject) => {
+
+    fetch(url)
     .then(function(response) {
       if (response.ok) {
-        console.log('File saved successfully.');
+        console.log('【 saveFile() 】File saved successfully.');
+
         // Perform any additional actions or show success message
+        resolve();
       } else {
         console.error('Failed to save file.');
         // Handle the error
+        reject();
       }
     })
     .catch(function(error) {
       console.error('An error occurred:', error);
       // Handle the error
+      reject();
     });
+
+  })
 }
 
-function createTextFile() {
 
+// Transform txt to png by calling server
+function font2img() {
+  // const front_url = construct_url();
+  // const req_url = `font2img`;
+  // const url = front_url.concat(req_url)
+  // console.log("obtained url: ", url)
+
+  return new Promise((resolve, reject) => {
+
+    fetch('/font2img')
+    .then(function(response) {
+      if (response.ok) {
+        console.log('【 font2img() 】txt transformed to png sucessfully');
+        // Perform any additional actions or show success message
+        resolve();
+      } else {
+        console.error('Failed to transform txt.');
+        // Handle the error
+        reject();
+      }
+    })
+    .catch(function(error) {
+      console.error('An error occurred:', error);
+      // Handle the error
+      reject();
+    });
+
+  })
 }
 
-async function displayImg(event) {
-  event.preventDefault();
+
+// Generate samples from server
+function generate_result() {
+  // const front_url = construct_url();
+  // const req_url = `generate_result`;
+  // const url = front_url.concat(req_url)
+  // console.log("obtained url: ", url)
+  console.log('【 generate_result() 】initiating generation.....');
+
+  return new Promise((resolve, reject) => {
+
+    fetch('/generate_result')
+    .then(function(response) {
+      if (response.ok) {
+        console.log('【 generate_result() 】result generated!');
+        // Perform any additional actions or show success message
+        resolve();
+      } else {
+        console.error('Failed to generate result.');
+        // Handle the error
+        reject();
+      }
+    })
+    .catch(function(error) {
+      console.error('An error occurred:', error);
+      // Handle the error
+      reject();
+    });
+
+  })
+}
+
+
+function display_result() {
   
-  // Get user input
-  const charInput = document.querySelector('.char-input');
-  const userInput = charInput.value;
+  fetch('/get_image')
+    .then(response => response.json())
+    .then(data => {
+        const result_img = document.getElementById('output-img');
+        result_img.src = `/image/${data.image_name}`;
+        console.log('【 display_result() 】got result img');
 
-  try {
-    const response = await fetch(`/files/${userInput}`);
-    if (!response.ok) {
-        throw new Error('File not found');
-    }
+        document.getElementById("output-loader").style.display = "none";
+        document.getElementById("output-result").style.display = "block";
+    })
+    .catch(error => console.error('Error fetching the result image:', error));
 
-    // Create a URL for the image and set it as the src of the image element
-    const imageBlob = await response.blob();
-    const imageURL = URL.createObjectURL(imageBlob);
-
-    // Show the image 
-    const outputImg = document.querySelector('#output-img');
-    outputImg.setAttribute('src', imageURL);
-} catch (error) {
-    console.error('Error fetching the image:', error);
 }
 
+
+function construct_url() {
+
+  const host = window.location.hostname;
+  const port = 9000; 
+  const url = `http://${host}:${port}/`;
+  console.log('【 construct_url() 】url for site constructed: ', url);
+
+  return url;
+
+}
+
+
+function update_loader(status) {
+
+  document.getElementById("output-result").style.display = "none";
+  document.getElementById("output-loader").style.display = "block";
+
+  // Update loader text to current status
+  document.getElementById("status").innerText = status;
 }
